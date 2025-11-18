6bbdb99aa201a7a96a9a095c25e3ed5c4c7078f7
Brian Stull <brain.stull@gmail.com>
2025-11-18 16:05:25 -0500

    server test program

---
+++ sse_server_test.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stddef.h>
+#include <pthread.h>
+#include <unistd.h>
+
+#include "sse_server.h"
+/*
+int send_sse_event(int client_fd, const char *data);
+ST_CLIENT_WRITER client_writer_init(char **data_queue);
+void client_writer_start(ST_CLIENT_WRITER *client_writer);
+void client_writer_stop(ST_CLIENT_WRITER *client_writer);
+void client_writer_add_client(ST_CLIENT_WRITER *client_writer, int client_fd);
+*/
+
+#define PORT 6262
+#define DATA_Q_SIZE 10
+
+int main(int argc, char *argv[]) {
+	int i;
+	pthread_t server_tid;
+	ST_CLIENT_WRITER *cw;
+	ST_SSE_SERVER *server;
+	
+	server = sse_server_init(PORT, DATA_Q_SIZE);
+	server_tid = sse_server_start(server);
+	
+	printf("Test server started...\n");
+	sleep(2);
+	printf("Slept for two seconds... adding data\n");
+	sse_server_queue_data(server, "data 1");
+	sse_server_queue_data(server, "data 2\n");
+	sse_server_queue_data(server, "data 3");
+
+	sse_server_stop(server);
+	exit(0);
+} 
