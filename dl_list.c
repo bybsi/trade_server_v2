871b3fe34cc2e04feb3f8c0d57fe8ef6473393f6
Brian Stull <brain.stull@gmail.com>
2025-11-24 22:05:27 -0500

    double linked list

---
+++ dl_list.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+DL_LIST *dl_list_init() {
+	DL_LIST *dl_list = malloc(sizeof(DL_LIST));
+	dl_list->head = dl_list_new_node(NULL, NULL);
+	dl_list->tail = dl_list->head;
+	dl_list->iter = dl_list->head;
+	return dl_list;
+}
+
+DLL_NODE *dl_list_new_node(void *data, void (*data_callback)(void *data_node)) {
+	DLL_NODE *node = malloc(sizeof(DLL_NODE));
+	node->data = data;
+	node->prev = NULL;
+	node->next = NULL;
+
+        // So we can do something with the slot the data was
+        // allocated to, such as putting a pointer to it in the
+        // data object.
+	if (data_callback)
+		data_callback->(node);
+
+	return node;
+}
+
+void dl_list_insert(DL_LIST *dl_list, void *data, void (*data_callback)(void *data_node)) {
+	DLL_NODE *new_node = dl_list_new_node(data, data_callback);
+	new_node->prev = dl_list->tail;
+	dl_list->tail->next = new_node;
+	dl_list->tail = new_node;
+}
+
+
---
+++ dl_list.h
@@ -0,0 +1,21 @@
+#ifndef _DL_LIST_H_
+#define _DL_LIST_H_
+
+typedef struct dl_list_node_t {
+	void *data;
+	struct dl_list_node_t *prev;
+	struct dl_list_node_t *next;
+} DLL_NODE;
+
+typedef struct dl_list_t {
+	DLL_NODE *head;
+	DLL_NODE *tail;
+	DLL_NODE *iter;
+} DL_LIST;
+
+
+DL_LIST *dl_list_init();
+DLL_NODE *dl_list_new_node(void *data, void (*data_callback)(void *data_node));
+void dl_list_insert(DL_LIST *dl_list, void *data, void (*data_callback)(void *data_node));
+
+#endif // _DL_LIST_H__
